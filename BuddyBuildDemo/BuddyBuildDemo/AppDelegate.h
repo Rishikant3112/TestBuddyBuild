6cacb5a3ed3f4fa00bc4840756ad3650185a8a8f
Rishikant3112 <mrishikant@fxbytes.com>
2018-05-10 11:41:58 +0530

    Initial commit
    Test app to test working of buddy app

---
+++ BuddyBuildDemo/BuddyBuildDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuddyBuildDemo
+//
+//  Created by Fxbytes on 10/05/18.
+//  Copyright © 2018 Fxbytes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BuddyBuildDemo/BuddyBuildDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BuddyBuildDemo
+//
+//  Created by Fxbytes on 10/05/18.
+//  Copyright © 2018 Fxbytes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
